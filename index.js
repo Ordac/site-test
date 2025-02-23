2a006f49e4648dc97eba473e22581955e4c9376d
Ordac <theordaccat@gmail.com>
2025-02-22 20:50:51 -0500

    more options

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const decreaseButton = document.getElementById("btn-decrease");
 const resetButton = document.getElementById("btn-reset");
 const increaseButton = document.getElementById("btn-increase");
+
+const rootButton = document.getElementById("btn-root");
+const squareButton = document.getElementById("btn-square");
+
 const counterDisplay = document.getElementById("count");
 
 let count = 0;
@@ -24,6 +28,21 @@ function Reset()
     counterDisplay.textContent = count;
 }
 
+function Root()
+{
+    count = Math.sqrt(count);
+    counterDisplay.textContent = count;
+}
+
+function Square()
+{
+    count **= 2;
+    counterDisplay.textContent = count;
+}
+
 decreaseButton.onclick = Decrease;
 increaseButton.onclick = Increase;
 resetButton.onclick = Reset;
+
+rootButton.onclick = Root;
+squareButton.onclick = Square;
